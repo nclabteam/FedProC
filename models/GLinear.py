22330f5236368ad20341d3765269ee404161f770
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-01-10 11:46:03 +0900

    add GLinear model

---
+++ models/GLinear.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class GLinear(nn.Module):
+    def __init__(self, configs):
+        super().__init__()
+
+        self.seq_len = configs.input_len
+        self.pred_len = configs.output_len
+
+        self.Linear = nn.Linear(self.seq_len, self.seq_len)
+        self.GeLU = nn.GELU()
+        self.Hidden1 = nn.Linear(self.seq_len, self.pred_len)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.Linear(x)
+        x = self.GeLU(x)
+        x = self.Hidden1(x)
+        x = x.permute(0, 2, 1)
+        return x  # [Batch, Output length, Channel]
