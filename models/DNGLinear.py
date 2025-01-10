a68f769f19c3145a908a8b16d01e1865df9bf8d1
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-01-10 11:52:31 +0900

    add DNGLinear model

---
+++ models/DNGLinear.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+
+from .DLinear import DLinear
+from .NLinear import NLinear
+
+optional = {
+    "moving_avg": 25,
+}
+
+
+def args_update(parser):
+    parser.add_argument("--moving_avg", type=int, default=None)
+
+
+class DNGLinear(nn.Module):
+    """
+    Paper: https://arxiv.org/abs/2501.01087
+    Source: https://github.com/t-rizvi/GLinear/blob/main/models/WIthout_Normalization/DNGLinear.py
+    """
+
+    def __init__(self, configs):
+        super().__init__()
+
+        self.seq_len = configs.input_len
+        self.pred_len = configs.output_len
+        self.input_channels = configs.input_channels
+
+        self.nlinear = NLinear(configs)
+        self.dlinear = DLinear(configs)
+        self.LinrLyr = nn.Linear(self.seq_len, self.pred_len)
+
+        self.Linear = nn.Linear(self.seq_len, self.seq_len)
+        self.GeLU = nn.GELU()
+        self.Hidden1 = nn.Linear(self.seq_len, self.pred_len)
+
+    def forward(self, x):
+        x1 = self.nlinear(x)
+        x2 = self.dlinear(x)
+        x3 = x.permute(0, 2, 1)
+
+        x3 = self.Linear(x3)
+        x3 = self.GeLU(x3)
+        x3 = self.Hidden1(x3)
+
+        x3 = x3.permute(0, 2, 1)
+        x4 = self.LinrLyr(x.permute(0, 2, 1)).permute(0, 2, 1)
+
+        x = (x1 + x2 + x3 + x4) / 4
+        return x  # [Batch, Output length, Channel]
