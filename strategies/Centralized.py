5df92e1d1768e8b464e2f8efd23e607762c1c111
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-03-05 09:46:23 +0900

    add parallel training

--- strategies/Centralized.py
+++ strategies/Centralized.py
@@ -1,3 +1,7 @@
+from collections import deque
+
+import ray
+
 from .base import Client, Server
 
 
@@ -12,18 +16,57 @@ class Centralized(Server):
         self.fix_results()
 
     def train_clients(self):
-        self.model.train()
-        for client in self.client_data:
-            train_loader = client["dataloader"]
-            for _ in range(self.epochs):
-                self.train_one_epoch(
-                    model=self.model,
-                    dataloader=train_loader,
-                    optimizer=self.optimizer,
-                    criterion=self.loss,
-                    scheduler=self.scheduler,
-                    device=self.device,
-                )
+        if self.parallel:  # Use parallel execution with Ray
+            futures = []
+            idle_workers = deque(range(self.num_workers))
+            job_map = {}
+
+            i = 0
+            while i < len(self.client_data) or len(futures) > 0:
+                while i < len(self.client_data) and len(idle_workers) > 0:
+                    worker_id = idle_workers.popleft()
+                    client = self.client_data[i]
+                    train_loader = client["dataloader"]
+
+                    # Parallelized `train_one_epoch` execution
+                    future = train_one_epoch_remote.remote(
+                        model=self.model,
+                        dataloader=train_loader,
+                        optimizer=self.optimizer,
+                        criterion=self.loss,
+                        scheduler=self.scheduler,
+                        device=self.device,
+                        epochs=self.epochs,
+                    )
+
+                    job_map[future] = (client, worker_id)
+                    futures.append(future)
+                    i += 1
+
+                if len(futures) > 0:
+                    all_finished, futures = ray.wait(futures)
+                    for finished in all_finished:
+                        client, worker_id = job_map[finished]
+                        model_state, optimizer_state = ray.get(finished)
+
+                        # Assign updated model & optimizer back
+                        self.model.load_state_dict(model_state)
+                        self.optimizer.load_state_dict(optimizer_state)
+
+                        idle_workers.append(worker_id)
+
+        else:  # Run in serial mode
+            for client in self.client_data:
+                train_loader = client["dataloader"]
+                for _ in range(self.epochs):
+                    self.train_one_epoch(
+                        model=self.model,
+                        dataloader=train_loader,
+                        optimizer=self.optimizer,
+                        criterion=self.loss,
+                        scheduler=self.scheduler,
+                        device=self.device,
+                    )
 
     def receive_from_clients(self):
         self.client_data = []
@@ -46,3 +89,20 @@ class Centralized(Server):
 class Centralized_Client(Client):
     def variables_to_be_sent(self):
         return {"dataloader": self.load_train_data()}
+
+
+@ray.remote
+def train_one_epoch_remote(
+    model, dataloader, optimizer, criterion, scheduler, device, epochs
+):
+    model.train()
+    for _ in range(epochs):
+        for batch in dataloader:
+            optimizer.zero_grad()
+            inputs, targets = batch[0].to(device), batch[1].to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+        scheduler.step()
+    return model.state_dict(), optimizer.state_dict()
