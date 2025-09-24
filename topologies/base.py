13c2dcd5f8efdafa6bf5ce955a6e0e88643798a2
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-09-24 12:21:40 +0900

    Update base.py

--- topologies/base.py
+++ topologies/base.py
@@ -3,6 +3,23 @@ class Topology:
         self.num_nodes = num_nodes
         self.neighbors = self._gen()
 
+    def get_neighbors(self, id):
+        return self.neighbors[id]
+
+    def get_connectivity_info(self):
+        """Return information about the topology connectivity."""
+        return {
+            "topology_type": "k-connected",
+            "num_nodes": self.num_nodes,
+            "total_connections": sum(
+                len(neighbors) for neighbors in self.neighbors.values()
+            ),
+            "avg_connections_per_node": sum(
+                len(neighbors) for neighbors in self.neighbors.values()
+            )
+            / self.num_nodes,
+        }
+
     def _gen(self):
         raise NotImplementedError
 
