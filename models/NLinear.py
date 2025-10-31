a2212876a0e2cc31d844847c8b3cfcb770476e89
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-10-31 11:31:49 +0900

    remove param init

--- models/NLinear.py
+++ models/NLinear.py
@@ -8,19 +8,20 @@ class NLinear(nn.Module):
     """
 
     def __init__(self, configs):
-        super(NLinear, self).__init__()
-        self.seq_len = configs.input_len
-        self.pred_len = configs.output_len
-
-        self.Linear = nn.Linear(self.seq_len, self.pred_len)
-        self.Linear.weight = nn.Parameter(
-            (1 / self.seq_len) * torch.ones([self.pred_len, self.seq_len])
-        )
+        super().__init__()
+        self.Linear = nn.Linear(configs.input_len, configs.output_len)
 
     def forward(self, x):
-        # x: [B, seq_len, in_channels]
+        # x: [batch_size, input_len, in_channels]
         seq_last = x[:, -1:, :].detach()
         x = x - seq_last
-        x = self.Linear(x.permute(0, 2, 1)).permute(0, 2, 1)
+        # x: [batch_size, in_channels, input_len]
+        x = x.permute(0, 2, 1)
+        # x: [batch_size, in_channels, input_len]
+        x = self.Linear(x)
+        # x: [batch_size, in_channels, output_len]
+        x = x.permute(0, 2, 1)
+        # x: [batch_size, output_len, in_channels]
         x = x + seq_last
-        return x  # [B, pred_len, out_channels]
+        # x: [batch_size, output_len, in_channels]
+        return x  
